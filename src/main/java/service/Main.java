10d96ae9277964fb2d953606d73a76cbb3cd6598
ignacio degeneffe <degeneffe@gmail.com>
2017-10-14 22:47:25 +0200

    update

--- src/main/java/service/Main.java
+++ src/main/java/service/Main.java
@@ -1,16 +1,29 @@
 package service;
+
 import static spark.Spark.*;
 
+import java.util.logging.Logger;
+
+import com.google.gson.Gson;
+
+import service.api.UserOrder;
 import service.businesslogic.IUserOrderService;
 import service.framework.BaseContextClass;
 import service.resource.MainResource;
 
-public class Main extends BaseContextClass{
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> {
-        	IUserOrderService uos = (IUserOrderService)MainResource.getBean("UserOrderService");
-    		log.info(uos.findUserOrderByEmailOrder("pedro@gmail.com", "miId").toString());
-        	return uos.findUserOrderByEmailOrder("pedro@gmail.com", "miId").toString();
-        });
-    }
+public class Main {
+	private static Gson gson = new Gson();
+	private static Logger log = Logger.getLogger("serviceApi");
+	
+	public static void main(String[] args) {
+		get("/hello", (req, res) -> {
+			IUserOrderService uos = (IUserOrderService) BaseContextClass.getBean("UserOrderService");
+			
+			UserOrder result = uos.findUserOrderByEmailOrder("pedro@gmail.com", "miId");
+			
+			log.info(result.toString());
+
+			return gson.toJson(result, UserOrder.class);
+		});
+	}
 }
--- src/main/java/service/businesslogic/UserOrderServiceImpl.java
+++ src/main/java/service/businesslogic/UserOrderServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import service.api.UserOrder;
 import service.domain.Order;
 import service.domain.User;
+import service.framework.BaseContextClass;
 import service.gateway.IExternalServiceOrderGateway;
 import service.gateway.IExternalServiceUserGateway;
 import service.gateway.ServicioExternoOrderImpl;
@@ -15,19 +16,21 @@ import service.gateway.ServicioExternoUserImpl;
 
 @Service("UserOrderService")
 public class UserOrderServiceImpl implements IUserOrderService {
- 
-	
-	
+
 	@Override
 	public UserOrder findUserOrderByEmailOrder(String strEmail, String strOrderId) {
 		// TODO Orquestación del servicio. Se llama a los dos servicios externos
 		// y se devuelve un UserOrder
 		User user = new User(strEmail, "", "", "");
-		IExternalServiceUserGateway serviceUser = new ServicioExternoUserImpl();
 
+		IExternalServiceUserGateway serviceUser = (IExternalServiceUserGateway) BaseContextClass
+				.getBean("ServicioExternoUser");
 		Order order = new Order(serviceUser.getUser(user).getStrUserId(), strOrderId, "");
-		IExternalServiceOrderGateway serviceOrder = new ServicioExternoOrderImpl();
+
+		IExternalServiceOrderGateway serviceOrder = (IExternalServiceOrderGateway) BaseContextClass
+				.getBean("ServicioExternoOrder");
 		order = serviceOrder.getOrder(order);
+
 		return new UserOrder(strEmail, user.getStrName(), user.getStrSurname(), user.getStrUserId(),
 				order.getStrOrderId(), order.getStrFlag());
 	}
